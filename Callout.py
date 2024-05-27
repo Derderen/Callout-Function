a4c7d3b1c45bb86e2549e8fa8173e83b3bf29dca
Deren <109401489+Derderen@users.noreply.github.com>
2024-05-27 19:39:15 +0700

    Create Callout.py

---
+++ Callout.py
@@ -0,0 +1,11 @@
+name = ["Steve", "Bob", "Maria", "Philip"]
+number = [0,2]
+
+def number_name(name,number):
+  result=[]
+  for x in number:
+      result.append(name[x])
+  return hasil
+
+output = number_name(name,number)
+print(output)
